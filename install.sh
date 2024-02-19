cf1d2bfb80fa1dffb8f7ffba0c3f8701884d715c
David Garcia <david_5043@hotmail.com>
2024-02-19 12:25:31 -0600

    added some things

--- install.sh
+++ install.sh
@@ -21,6 +21,12 @@ fi
 if [[ `uname` == "Darwin"   ]]; then
   echo "MacOs detected. Using Darwin config..."
 
+  echo "Installing Oh my zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  echo "Installing powerlevel10k theme"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
   echo "Installing brew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -49,20 +55,22 @@ if [[ `uname` == "Darwin"   ]]; then
   brew install --cask font-cascadia-mono
   brew install --cask font-cascadia-mono-pl
 
+  echo "Installing MesloLGS NF fonts (powerlink10k)"
+  brew install --cask font-meslo-lg-nerd-font
+
   echo "Installing plugins"
-  brew install zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
-echo "Removing existing dotfiles"
 # remove files if they already exist
+echo "Removing existing dotfiles"
 rm -rf ~/.zshrc
 
 echo "Creating new .zshrc"
 touch ~/.zshrc
 
 echo "Setting up .zshrc"
-cp .zshrc ~/.zshrc
+cp zshrc ~/.zshrc
 
 echo "Apllying all configurations"
 source ~/.zshrc
