fb500c81b1802cfb6e46735033c6fc367cefe4e7
David Garcia <david_5043@hotmail.com>
2024-02-19 12:42:50 -0600

    added plugins

--- install.sh
+++ install.sh
@@ -59,7 +59,10 @@ if [[ `uname` == "Darwin"   ]]; then
   brew install --cask font-meslo-lg-nerd-font
 
   echo "Installing plugins"
+  echo "zsh-syntax-highlighting"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  echo "zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 # remove files if they already exist
